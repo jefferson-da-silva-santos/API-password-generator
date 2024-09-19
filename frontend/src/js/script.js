573b254c5a6a944f4d7c3aeb805badfdeadf973c
jefferson-da-silva-santos <jeffrrWPG678@gmail.com>
2024-09-19 15:01:58 -0300

    Logica de abrir/fechar menu implementada

--- frontend/src/js/script.js
+++ frontend/src/js/script.js
@@ -1,9 +1,24 @@
 const inputText = document.getElementById('input-value');
-const inputRange = document.getElementById('input-range')
+const inputRange = document.getElementById('input-range');
+//menu variaveis
+const btnMenu = document.querySelector('.menu');
+const listMenu = document.querySelector('.nav__list-menu');
+const divSeparator = document.querySelector('.separator');
+let menuVisible = false;
 
 document.addEventListener('DOMContentLoaded', () => {
   inputText.value = inputRange.value;
+
+  btnMenu.addEventListener('click', () => {
+    listMenu.style.display = !menuVisible ? 'flex':'none';
+    toogleMenuVisible();
+    document.body.style.overflow = !menuVisible ? 'auto':'hidden';
+  })
 });
+
+function toogleMenuVisible() {
+  menuVisible = !menuVisible;
+}
 
 function updateValueRange() {
   inputText.value = inputRange.value;
